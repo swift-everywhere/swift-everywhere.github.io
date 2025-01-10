8bc8109ce9c1c613968f454aa24b2330f0727ddb
Marc Prud'hommeaux <marc@skip.tools>
2025-01-10 11:01:38 -0500

    Update build status

--- scripts/updatebuilds.sh
+++ scripts/updatebuilds.sh
@@ -2,13 +2,13 @@
 SCRIPT=$(realpath $0)
 cd $(dirname ${SCRIPT})/..
 
+PACKAGES="../swift-package-builds/index/packages.json"
+INDEX="../swift-package-builds/index/android.json"
+STATS="_data/android_stats.json"
+
 cd ../swift-package-builds
 git pull
 cd -
-
-INDEX="../swift-package-builds/index/android.json"
-#STATS="../swift-package-builds/index/android_stats.json"
-STATS="_data/android_stats.json"
 
 cat ${INDEX} | jq 'to_entries | map(.value) | sort_by(.stars) | reverse' > _data/android.json
 
@@ -16,7 +16,6 @@ echo "{}" > ${STATS}
 
 COUNT=$(cat ${INDEX} | jq 'length')
 jq --arg COUNT "$COUNT" '."total_packages" = $COUNT' $STATS > $STATS.new && mv $STATS.new $STATS
-
 
 SUCCESS=$(cat ${INDEX} | jq '[.[] | select(.status == "success")] | length')
 jq --arg SUCCESS "$SUCCESS" '."success_packages" = $SUCCESS' $STATS > $STATS.new && mv $STATS.new $STATS
